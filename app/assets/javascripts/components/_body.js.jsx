4a41f34ad63562759b049259668804059383e80b
Sarath Mantrala <sarath23@gmail.com>
2017-05-03 22:13:47 -0700

    Added react-rails gems to the project

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,9 @@
+var Body = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Contactually body</h1>
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Contactually header</h1>
+      </div>
+    )
+  }
+});
