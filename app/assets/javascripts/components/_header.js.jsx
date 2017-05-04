d205f1aabd97a32a43bc39a6cbc212ea49a6dae3
Sarath Mantrala <sarath23@gmail.com>
2017-05-03 22:28:58 -0700

    Added bootstrap gem

--- app/assets/javascripts/components/_header.js.jsx
+++ app/assets/javascripts/components/_header.js.jsx
@@ -1,9 +1,15 @@
 var Header = React.createClass({
   render() {
     return (
-      <div>
-        <h1>Contactually header</h1>
+      <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <h1>Contactually</h1>  
+          </a>
+        </div>
       </div>
+    </nav>
     )
   }
 });
