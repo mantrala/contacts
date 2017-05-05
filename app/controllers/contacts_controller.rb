0a33bacc7f8b457c63637ba35ce984173fd7052d
Sarath Mantrala <sarath23@gmail.com>
2017-05-05 09:25:19 -0700

    Only csv files can be uploaded now * Polished the flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,16 @@ class ContactsController < ApplicationController
 
   # can move this to be a background job for faster req processing
   def upload
+    if params[:file] && params[:file].content_type != "text/csv"
+      flash[:error] = "Only csv files are allowed"
+      redirect_to root_url and return
+    end
+
     begin
       ContactImporter.import(params[:file])
       redirect_to root_url, notice: "Contacts imported."
     rescue
-      redirect_to root_url, notice: "Upload failed. The dev team has been notified of it."
+      redirect_to root_url, error: "Upload failed. The dev team has been notified of it."
     end
   end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -14,15 +14,26 @@ RSpec.describe ContactsController, type: :controller do
   end
 
   describe 'a POST to #upload with a CSV file' do
-    let(:file) { fixture_file_upload 'files/data.csv' }
+    let(:file) { fixture_file_upload 'files/data.csv', 'text/csv' }
     before { post :upload, file: file }
 
-    it 'responds with 200' do
+    it 'responds with 302' do
       expect(response).to have_http_status(302)
     end
 
     it 'imports the contacts in the file' do
       expect(Contact.count).to eq(200)
+    end
+
+  end
+
+  describe 'a POST to #upload ' do
+    let(:file) { fixture_file_upload 'files/Rakefile', 'text/xml' }
+
+    it 'rejects import from if not from a csv' do
+      post :upload, file: file
+      expect(flash[:error]).to be_present
+      expect(flash[:error]).to eq('Only csv files are allowed')
     end
   end
 
